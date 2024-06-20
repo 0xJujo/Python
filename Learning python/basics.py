914ebcc9677cb4bf63e5a34f2d7191473327d91f
Nishit <85806312+0xJujo@users.noreply.github.com>
2024-06-20 18:16:33 +0530

    Second commit today

---
+++ Learning python/basics.py
@@ -0,0 +1,37 @@
+print("Hello, World!")
+print("Hello, World!")
+
+'''this is an example of a multi line comment in python'''
+
+# this is a single line comment, select the lines to comment and use control + / to comment out the lines
+
+# variables in python
+
+a= 3
+print(a)
+b=4
+print(b)
+
+c= True
+print(c)
+
+d= "hello"
+print(d)
+
+a= 3.14
+print(a)
+
+e=None
+print(e) # None is a special data type in python that is used to define a null value or no value at all
+# above are examples of different types of variables in python can override the data type of a variable too
+
+# to check the data type of a variable use the type() function
+print(type(a))
+
+#can even do typecasting by changing the data type of a variable manually to perform operations
+
+f="3"
+print(type(f))
+print(type(int(f)))
+print(int(f)+1)
+
---
+++ Learning python/operators.py
@@ -0,0 +1,27 @@
+num1=50
+num2=3
+
+#arithmetic operators
+print("num1 + num2",num1+num2)
+print("num1 - num2",num1-num2)
+print("num1 * num2",num1*num2)
+print("num1 / num2",num1/num2)
+print("num1 // num2",num1//num2)
+print("num1 ** num2",num1**num2)
+print("num1 % num2",num1%num2)
+
+#comparison operators
+
+print (num1==num2)
+print (num1!=num2)
+print (num1>num2)
+print (num1<num2)
+
+#Assignment operators
+num1+=num2
+print(num1)
+
+#logical operators
+print(num1>num2 and num1<num2)
+print(num1>num2 or num1<num2)
+print(not(num1>num2 and num1<num2))
