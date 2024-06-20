ab5dc3f6de019071dc93a3cc3442b236d5aae012
Nishit <85806312+0xJujo@users.noreply.github.com>
2024-06-20 17:03:50 +0530

    First commit

---
+++ Learning python/helloworld.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+print("Hello, World!")
+
+"this is an example of a multi line comment in python"
+
+# this is a single line comment, select the lines to comment and use control + / to comment out the lines
+
+# variables in python
+
+a= 3
+print(a)
+b
+
